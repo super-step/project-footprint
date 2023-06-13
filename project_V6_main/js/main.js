ef55fcc416385bc0fad8243a485b1df0383f443c
dbsdud00 <dbsdudsw@icloud.com>
2023-06-13 12:55:45 +0900

    0613

--- project_V6_main/js/main.js
+++ project_V6_main/js/main.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const main_to_map = document.querySelector(".main_to_map img");
-
-  main_to_map.addEventListener(
-    "click",
-    window.scrollTo(0, document.body.scrollHeight)
-  );
+  const gj_map = document.querySelector('area[title="gj_map"]');
 });
