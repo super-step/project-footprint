eade68f2b1d7f7665eec3c20f1812d90846d696b
dbsdud00 <dbsdudsw@icloud.com>
2023-06-16 13:18:09 +0900

    0616

--- project_V8_sns/js/main.js
+++ project_V8_sns/js/main.js
@@ -1,12 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-  var container = document.getElementById("sns_api_map");
-  var options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3,
-  };
-
-  var map = new kakao.maps.Map(container, options);
-});
+document.addEventListener("DOMContentLoaded", () => {});
 
 // if ($('#location-map')) {
 //   $('#location-map area').each(function() {
