13593a347506a276c54e4737cea142a7bc1d1924
dbsdud00 <dbsdudsw@icloud.com>
2023-06-16 16:40:59 +0900

    06

--- project_V8_sns/js/map.js
+++ project_V8_sns/js/map.js
@@ -1,7 +1,21 @@
-var container = document.getElementById("sns_api_map");
-var options = {
-  center: new kakao.maps.LatLng(33.450701, 126.570667),
-  level: 3,
-};
+var mapContainer = document.querySelector(".sns_map"), // 지도를 표시할 div
+  mapOption = {
+    center: new kakao.maps.LatLng(35.1200598333, 126.9989650099), // 지도의 중심좌표
+    level: 3, // 지도의 확대 레벨
+  };
 
-var map = new kakao.maps.Map(container, options);
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커가 표시될 위치입니다
+var markerPosition = new kakao.maps.LatLng(35.1200598333, 126.9989650099);
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+  position: markerPosition,
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null);
