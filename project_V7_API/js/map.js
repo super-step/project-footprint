937dbd23737f45836a2fe7353f055703289b1d2a
dbsdud00 <dbsdudsw@icloud.com>
2023-06-16 15:35:03 +0900

    0616

--- project_V7_API/js/map.js
+++ project_V7_API/js/map.js
@@ -6,20 +6,10 @@ var options = {
 
 var map = new kakao.maps.Map(container, options);
 
-var positions = fetch("./data/gj_map_data.json");
-let names = [];
-let Latitude = [];
-let longitude = [];
-positions
-  .then((data) => data.json())
-  .then((obj) => {
-    names.push(obj.name);
-    Latitude.push(obj.Latitude);
-    longitude.push(obj.longitude);
-    console.log();
-  });
-console.log(names);
+let positions = JSON.parse(JSON.stringify(TestFile));
 
+console.log(positions);
+console.log(positions.gj);
 // var loca = [
 //   {
 //     name: "무등산 주상절리대",
@@ -75,12 +65,12 @@ console.log(names);
 
 // console.log(loca);
 // 마커 이미지의 이미지 주소입니다
-// var imageSrc =
-//   "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+var imageSrc =
+  "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
-var imageSrc = "./image/icon/down-arrow.png";
+// var imageSrc = "./image/icon/down-arrow.png";
 
-for (var i = 0; i < positions.length; i++) {
+for (var i = 0; i < positions.gj.length; i++) {
   // 마커 이미지의 이미지 크기 입니다
   var imageSize = new kakao.maps.Size(24, 35);
 
@@ -91,10 +81,10 @@ for (var i = 0; i < positions.length; i++) {
   var marker = new kakao.maps.Marker({
     map: map, // 마커를 표시할 지도
     position: new kakao.maps.LatLng(
-      positions[i].Latitude,
-      positions[i].longitude
+      positions.gj[i].Latitude,
+      positions.gj[i].longitude
     ), // 마커를 표시할 위치
-    title: positions[i].name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+    title: positions.gj[i].name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
     image: markerImage, // 마커 이미지
   });
 }
