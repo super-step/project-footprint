8c5b6e204682e74e6e5ad24af9dee9453b6cbd00
dbsdud00 <dbsdudsw@icloud.com>
2023-06-16 14:57:26 +0900

    0616

--- project_V7_API/js/map.js
+++ project_V7_API/js/map.js
@@ -1,4 +1,3 @@
-// var container = document.getElementById("sns_api_map");
 var container = document.querySelector(".API_map");
 var options = {
   center: new kakao.maps.LatLng(35.16, 126.8512),
@@ -7,66 +6,71 @@ var options = {
 
 var map = new kakao.maps.Map(container, options);
 
-var positions = fetch("./data/광주_관광지_데이터.json");
+var positions = fetch("./data/gj_map_data.json");
+let Latitude = [];
+let longitude = [];
 positions
   .then((data) => data.json())
   .then((obj) => {
-    console.log(obj);
+    Latitude.push(obj.Latitude);
+    longitude.push(obj.longitude);
   });
+console.log(positions);
 
-var location = [
-  {
-    name: "무등산 주상절리대",
-    Latitude: 35.1200598333,
-    longitude: 126.9989650099,
-  },
-  {
-    name: "중외공원",
-    Latitude: 35.1814981787,
-    longitude: 126.8842958259,
-  },
-  {
-    name: "사직공원(광주)",
-    Latitude: 35.1413559608,
-    longitude: 126.9121749221,
-  },
-  {
-    name: "광주호",
-    Latitude: 35.1851523796,
-    longitude: 126.9927528112,
-  },
-  {
-    name: "약사암(광주)",
-    Latitude: 35.1223174593,
-    longitude: 126.9722478684,
-  },
-  {
-    name: "원효사",
-    Latitude: 35.1486258304,
-    longitude: 126.9857793462,
-  },
-  {
-    name: "증심사(광주)",
-    Latitude: 35.1287583574,
-    longitude: 126.9697618157,
-  },
-  {
-    name: "충장사",
-    Latitude: 35.1625602481,
-    longitude: 126.9787157312,
-  },
-  {
-    name: "포충사",
-    Latitude: 35.0898468165,
-    longitude: 126.8487457033,
-  },
-  {
-    name: "충민사",
-    Latitude: 35.1519696177,
-    longitude: 126.9692640546,
-  },
-];
-// console.log(location);
+// var loca = [
+//   {
+//     name: "무등산 주상절리대",
+//     Latitude: 35.1200598333,
+//     longitude: 126.9989650099,
+//   },
+//   {
+//     name: "중외공원",
+//     Latitude: 35.1814981787,
+//     longitude: 126.8842958259,
+//   },
+//   {
+//     name: "사직공원(광주)",
+//     Latitude: 35.1413559608,
+//     longitude: 126.9121749221,
+//   },
+//   {
+//     name: "광주호",
+//     Latitude: 35.1851523796,
+//     longitude: 126.9927528112,
+//   },
+//   {
+//     name: "약사암(광주)",
+//     Latitude: 35.1223174593,
+//     longitude: 126.9722478684,
+//   },
+//   {
+//     name: "원효사",
+//     Latitude: 35.1486258304,
+//     longitude: 126.9857793462,
+//   },
+//   {
+//     name: "증심사(광주)",
+//     Latitude: 35.1287583574,
+//     longitude: 126.9697618157,
+//   },
+//   {
+//     name: "충장사",
+//     Latitude: 35.1625602481,
+//     longitude: 126.9787157312,
+//   },
+//   {
+//     name: "포충사",
+//     Latitude: 35.0898468165,
+//     longitude: 126.8487457033,
+//   },
+//   {
+//     name: "충민사",
+//     Latitude: 35.1519696177,
+//     longitude: 126.9692640546,
+//   },
+// ];
+
+// console.log(loca);
 // 마커 이미지의 이미지 주소입니다
 // var imageSrc =
 //   "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
