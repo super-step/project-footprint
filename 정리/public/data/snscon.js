6a0c763a8e2acbc9d3c6f1408f35059dc988d3c4
dbsdud00 <dbsdudsw@icloud.com>
2023-06-26 15:55:02 +0900

    0626

--- 정리/public/data/snscon.js
+++ 정리/public/data/snscon.js
@@ -15,15 +15,15 @@ const arrSnsCotent = [
     content:
       "5.18 당시 희생된 초,중,고 학생들의 숭고한 정신을 계승하고 추모하기위해 '하늘을 보라-염' 주제의 동상.",
     img: "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA1MjlfMjQ0%2FMDAxNjg1MzQ4MDcyMjE4.0HIiYiJuN8snciuzwloHiGNP1ciZUCth-N5Qrf_qClEg.lePVbfs02PumIZ-liSioYGPuNO4imwshLrxAb-Qu-9wg.JPEG.doggum28%2F%25B1%25A4%25C1%25D6%25B1%25E2%25B0%25F811.JPG",
-    heart: 10,
+    heart: 20,
   },
   {
     mName: "운천저수지",
-    userId: "newid",
+    userId: "test",
     title: "벚꽃명소 운천저수지",
     content: "근처에 숙소잡고 멀리나가기전에 산책하기 좋아요~",
     img: "https://www.focusi.co.kr/news/photo/202003/146455_165903_4849.jpg",
-    heart: 10,
+    heart: 30,
   },
 ];
 
--- 정리/public/js/mypage/myPage.js
+++ 정리/public/js/mypage/myPage.js
@@ -2,16 +2,70 @@ let myName = document.querySelector(".mPProfileName");
 let myNic = document.querySelector(".mPNowNic");
 let myNicChange = document.querySelector(".mPNicChange");
 let myLogout = document.querySelector(".mPProfileLogout");
+let myPWChange = document.querySelector(".mPPWChange");
+let modalValueChange = document.querySelector("#my_modal_change_btn");
+let like1 = document.querySelector(".mPLikeTitle:");
+
 let member = JSON.parse(sessionStorage.getItem("member"));
 const sessionUser = JSON.parse(sessionStorage.getItem("user"));
-
 myName.innerHTML = `${sessionUser.name}`;
+console.log(sessionUser.nick);
 myNic.innerHTML = `${sessionUser.nick}`;
-myNicChange.addEventListener("click", () => {});
+myNicChange.addEventListener("click", () => {
+  let modal_title = document.querySelector(".my_modal_title h2");
+  modal_title.innerHTML = "닉네임 변경";
+  modal.style.display = "flex";
+
+  modalValueChange.addEventListener("click", () => {
+    let myChangeValue = document.querySelector(
+      "input[name=myChangeValue]"
+    ).value;
+    console.log(myChangeValue);
+
+    sessionUser.nick = myChangeValue;
+    console.log(sessionUser.nick);
+
+    alert(`변경되었습니다.${myChangeValue}`);
+
+    sessionStorage.setItem("user", JSON.stringify(sessionUser));
+  });
+});
 myLogout.addEventListener("click", () => {
   sessionStorage.removeItem("user");
   document.location.href = "./index.html";
 });
+myPWChange.addEventListener("click", () => {
+  let modal_title = document.querySelector(".my_modal_title h2");
+  modal_title.innerHTML = "비밀번호 변경";
+  modal.style.display = "flex";
+
+  modalValueChange.addEventListener("click", () => {
+    let myChangeValue = document.querySelector(
+      "input[name=myChangeValue]"
+    ).value;
+    console.log(myChangeValue);
+
+    sessionUser.password = myChangeValue;
+    console.log(sessionUser.password);
+
+    alert(`변경되었습니다.${sessionUser.password}`);
+
+    sessionStorage.setItem("user", JSON.stringify(sessionUser));
+  });
+});
+
+// modalValueChange.addEventListener("click", () => {
+//   let myChangeValue = document.querySelector("input[name=myChangeValue]").value;
+//   console.log(myChangeValue);
+
+//   sessionUser.nick = myChangeValue;
+//   console.log(sessionUser.nick);
+
+//   alert(`변경되었습니다.${myChangeValue}`);
+
+//   sessionStorage.setItem("user", JSON.stringify(sessionUser));
+// });
+
 // 세션유무에 따른 메뉴바 처리
 // let isLogin = false;
 // const menuBar = document.querySelector("nav.cyy_nav");
