210a89f23d022dfdda9219af44f1d37cea7afff1
dbsdud00 <dbsdudsw@icloud.com>
2023-06-27 08:14:52 +0900

    data

--- 정리/public/data/member.js
+++ 정리/public/data/member.js
@@ -6,6 +6,27 @@ const members = [
     nick: "고양이",
     tel: "010-1111-1111",
   },
+  {
+    id: "user1",
+    password: "1234",
+    name: "오명훈",
+    nick: "사막여우",
+    tel: "010-2222-1111",
+  },
+  {
+    id: "user2",
+    password: "1234",
+    name: "김선재",
+    nick: "침팬치",
+    tel: "010-1111-2222",
+  },
+  {
+    id: "user3",
+    password: "1234",
+    name: "최윤영",
+    nick: "고양이",
+    tel: "010-2222-3333",
+  },
 ];
 let member = sessionStorage.getItem("member");
 if (member === null) {
