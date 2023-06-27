dcc9892b5cee08f0768ceabc409ae06b1a395b16
dbsdud00 <dbsdudsw@icloud.com>
2023-06-27 13:05:19 +0900

    0627

--- 정리/public/js/mypage/myPage_PostManage.js
+++ 정리/public/js/mypage/myPage_PostManage.js
@@ -25,14 +25,20 @@ snsCotents.forEach((element) => {
   if (element.userId == sessionUser.id) {
     nowHeart = nowHeart + element.heart;
     const PostboxString = `
-    <div class="mPPostBox">
-    <img class="mPPostImg" src="${element.img}" alt="" />
+
+  <div class="mPPostBox">
+  <img class="mPPostImg" src="${element.img}" alt="" />
+  <div class="mPPostTitlebox">
+    <div class="mPPostMapTitle">${element.mName}</div>
     <div class="mPPostHeart">
       <span class="mPPostHeartCount">${element.heart}</span>
       <img src="../image/icon_heart.png" alt="" />
     </div>
-    <div class="mPPostTitle">${element.title}</div>
   </div>
+  <div class="mPPostTitle">${element.title}</div>
+</div>
+
+
     `;
     totalPostList = totalPostList + PostboxString;
 
